da61eae1e9781dc5d2ea63690c06fc5bb681e7ae
Ky Eltis <skylite.eltis@gmail.com>
2022-02-11 13:24:10 +1000

    Update at Fri 11 Feb 2022 13:24:10 AEST

--- PE BASE.py
+++ PE BASE.py
@@ -1,17 +1,20 @@
 """Project Euler <num>."""
 
 try:
-    """Works if ../Modules is in MYPYPATH or PYTHONPATH env vars"""
-    from timer import timer
+    """Works if Python-Debugging-Modules/ is in MYPYPATH or PYTHONPATH"""
+    from Modules.timer import timer
+    from Modules.debugger import Debugger
+    # from Modules.plotter import plotter
 except ModuleNotFoundError:
+    """Temporarily adds modules to Python PATH"""
     import os
     import sys
-    script_path = os.path.realpath(os.path.dirname(__name__))
-    os.chdir(script_path)
-    sys.path.append("./Python-Debugging-Modules/")
-    from timer import timer  # type: ignore
-from debugger import debug, debug_table  # type: ignore
-# from plotter import plotter  # type: ignore
+    script_path = os.path.abspath(os.path.dirname(__file__))
+    module_path = os.path.join(script_path, "Modules")  # Change to module path
+    sys.path.append(module_path)
+    from timer import timer
+    from debugger import Debugger
+    # from plotter import plotter
 # from numba import jit
 # from math import pi, sin, cos
 # import numpy as np
@@ -22,11 +25,12 @@ from debugger import debug, debug_table  # type: ignore
 # @jit(nopython=True, fastmath=True, nogil=True)  # , parallel=True)
 def main():
     """Run thing."""
+    d = Debugger()
     for i in range(10):
         var1 = i * 2
         var2 = i * 3
-        debug(locals(), "[var1,var2]")
-    print(debug_table())
+        d.debug(locals(), "[var1,var2]")
+    d.table()
     pass
 
 
